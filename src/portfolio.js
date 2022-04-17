0837e1ce4b4c88b2a7d3b2e213b7119b1fa38e19
sufyan mughal <captainsufyan158005@gmail.com>
2022-04-18 03:22:09 +0500

    first updates in name and text

--- src/portfolio.js
+++ src/portfolio.js
@@ -11,10 +11,10 @@ const illustration = {
 };
 
 const greeting = {
-  username: "Saad Pasta",
-  title: "Hi all, I'm Saad",
+  username: "Sufyan Ahmad",
+  title: "Hi all, I'm Sufyan Ahmad",
   subTitle: emoji(
-    "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
+    "A passionate Full Stack Web Designer ,Developer 🚀 having an experience of building Websites and Mobile applications with Wordpress + Elementor + Divi + Avada + WooCommerce / Shopify / Wix / squarespace / Html5 / css3 / Bootstrap / jquery //JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
   ),
   resumeLink:
     "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing", // Set to empty to hide the button
