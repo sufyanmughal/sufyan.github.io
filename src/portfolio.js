d0af32522146def6b5641f360c536240bbb07615
sufyan mughal <captainsufyan158005@gmail.com>
2022-04-18 05:43:29 +0500

    loop

--- src/portfolio.js
+++ src/portfolio.js
@@ -20,7 +20,7 @@ const illustration = {
 };
 
 const greeting = {
-  username: "Sufyan Ahmad",
+  username: "Sufyan",
   title: "Hi all, I'm Sufyan Ahmad",
   subTitle: emoji(
     "A passionate Full Stack Web Designer ,Developer 🚀 having an experience of building Websites and Mobile applications with Wordpress + Elementor + Divi + Avada + WooCommerce / Shopify / Wix / squarespace / Html5 / css3 / Bootstrap / jquery //JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
