6740ecba9960a8760125e6f62214fd6a54c8e6df
Abu sufyan <captainsufyan158005@gmail.com>
2022-04-24 07:13:24 +0500

    social links updated

--- src/portfolio.js
+++ src/portfolio.js
@@ -33,13 +33,13 @@ const greeting = {
 // Social Media Links
 
 const socialMediaLinks = {
-  github: "https://github.com/saadpasta",
-  linkedin: "https://www.linkedin.com/in/saadpasta/",
-  gmail: "saadpastall70@gmail.com",
+  github: "https://github.com/sufyanmughal",
+  linkedin: "https://www.linkedin.com/in/abu-sufyan-9mn/",
+  gmail: "braintechlogics@gmail.com",
   gitlab: "https://gitlab.com/saadpasta",
-  facebook: "https://www.facebook.com/saad.pasta7",
+  facebook: "https://web.facebook.com/sufyan.sufyan.12532/",
   medium: "https://medium.com/@saadpasta",
-  stackoverflow: "https://stackoverflow.com/users/10422806/saad-pasta",
+  stackoverflow: "https://stackoverflow.com/users/14568068/braintechlogics",
   // Instagram, Twitter and Kaggle are also supported in the links!
   // To customize icons and social links, tweak src/components/SocialMedia
   display: true // Set true to display this section, defaults to false
