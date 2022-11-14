257cdfc1576d7640804121715377beceebdfdf49
Abu sufyan <captainsufyan158005@gmail.com>
2022-11-15 02:42:14 +0500

    ..

--- src/portfolio.js
+++ src/portfolio.js
@@ -52,7 +52,7 @@ const skillsSection = {
   subTitle: "CRAZY FULL STACK DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK.........",
   skills: [
     emoji(
-      "⚡ I create landing pages, sales pages and websites that convert, on average, in the top 10% of their respective markets! When you need conversions, I'm here to get them for you. Sit back, relax, and know that your needs are being handled by one of the best. "
+      "⚡ I create landing pages, sales pages and websites that convert, on average, in the top 10% of their respective markets! When you need conversions, I'm here to get them for you. Sit back, relax, and know that your needs are being handled by one of the best"
     )
   ],
 
